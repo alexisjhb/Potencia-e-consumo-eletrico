caeda75215abc40ca6e0f493ec2a80341998180a
alexisjhb <169068450+alexisjhb@users.noreply.github.com>
2024-05-27 21:00:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Potência e Consumo");
+    System.out.println("");
+    System.out.println("Uma tv de 200 watts fica ligada aproximadamente 5h diariamente. Determine o consumo eletrico mensal da tv");
+    System.out.println("");
+    System.out.println("Valor da potência ");
+    int potencia = 200;
+    System.out.println(potencia);
+    System.out.println("");
+    System.out.println("Quantidade de horas ligada");
+    int horas = 5;
+    System.out.println(horas);
+    System.out.println("");
+      System.out.println("Horas . Potencia:");
+        int potenciaXhoras = potencia * horas;
+    System.out.println(potencia/horas);
+    System.out.println("");
+    System.out.println("1000 dividido por 1000:");
+    int mil = 1000;
+    int milmil = mil / mil;
+    System.out.println(milmil);
+    System.out.println("");
+    System.out.println("Consumo Mensal:");
+    System.out.println("30W");
+  }
+}
